e0e794a18f29ebcb32ff2013471d4967e28df017
Pranay Raut <pranayraut129@gmail.com>
2024-09-26 12:39:27 +0530

    Add project files.

---
+++ ReversedNumber/Model/Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedNumber.Model
+{
+    internal class Reverse
+    {
+        public static int ToReverse(int number)
+        {
+            int tempValue = number;
+            int reverse = 0;
+            int minValue = 0;
+            int divisor = 10;
+            int multiple = 10;
+
+            //Perfrom the reverse of number 
+            while (tempValue > minValue)
+            {
+                int lastNumber = tempValue % divisor;
+                reverse = (reverse * multiple) + lastNumber;
+                tempValue = tempValue / divisor;
+            }
+            return reverse;
+        }
+    }
+}
---
+++ ReversedNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using ReversedNumber.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            int number = int.Parse(Console.ReadLine());
+
+            int reverse = Reverse.ToReverse(number);
+
+            Console.WriteLine($"The Reverse of a number is {reverse}");
+        }
+    }
+}
